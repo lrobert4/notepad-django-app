0a82baad54567f2683e334d662b6c365311d8017
leer <ladesigns90@gmail.com>
2020-04-29 17:05:46 -0400

    Completed Note_list view

---
+++ src/notes/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+LABEL_CHOICES = (
+    
+    ('P', 'primary'), 
+    ('SE', 'secondary'), 
+    ('S', 'success'), 
+    ('D', 'danger'), 
+    ('W', 'warning'), 
+    ('I', 'info'), 
+    ('L', 'light'),
+    ('D', 'dark')  
+    
+)
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    due_date = models.DateTimeField()
+    label = models.CharField(choices=LABEL_CHOICES, max_length=2)
+
+    def __str__(self):
+        return self.title
