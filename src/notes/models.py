2b7efad331304c0592e0d9691f824b0fc4adbaae
leer <ladesigns90@gmail.com>
2020-04-30 12:59:28 -0400

    Made edits to the models syntax. Completed finishing up the views and CRUD operations.

--- src/notes/models.py
+++ src/notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 LABEL_CHOICES = (
     
@@ -19,6 +20,22 @@ class Note(models.Model):
     label = models.CharField(choices=LABEL_CHOICES, max_length=2)
     finished = models.BooleanField(default=False)
 
-    
+
     def __str__(self):
         return self.title
+
+    def get_finish_item_url(self):
+        return reverse("finish-note-item", kwargs={
+            'id': self.id
+        })
+
+    def get_recover_item_url(self):
+        return reverse("recover-note-item", kwargs={
+            'id': self.id
+        })
+
+    def get_delete_item_url(self):
+        return reverse("delete-note-item", kwargs={
+            'id': self.id
+        })
+
