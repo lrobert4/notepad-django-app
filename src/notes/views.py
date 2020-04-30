e3f68e25860abc6af7934f83e37b63a9f24d94ec
leer <ladesigns90@gmail.com>
2020-04-30 11:23:11 -0400

    Completed developing the Finish item view.

--- src/notes/views.py
+++ src/notes/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Note
 
 def note_list_view(request):
-    notes = Note.objects.all()
-    context = {'notes': notes}
+    to_do_list = Note.objects.filter(finished=False)
+    finished_list = Note.objects.filter(finished=True)
+    context = {
+        'to_do_list': to_do_list,
+        'finished_list': finished_list}
     return render(request, "note_list.html", context)
+
+def finish_item(request, id):
+    note = get_object_or_404(Note, id=id)
+    note.finished = True
+    note.save()
+    return redirect('note-list')
